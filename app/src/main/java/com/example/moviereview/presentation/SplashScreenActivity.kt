ea2acb7710b6eb93749182dae3b6e36edb27d2f9
dduckcat <deadduckcat@gmail.com>
2022-08-02 18:06:08 +0300

    commit

--- app/src/main/java/com/example/moviereview/presentation/SplashScreenActivity.kt
+++ app/src/main/java/com/example/moviereview/presentation/SplashScreenActivity.kt
@@ -1,12 +1,23 @@
 package com.example.moviereview.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.example.moviereview.R
+import com.example.moviereview.presentation.view.ActivityRecycler
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this, ActivityRecycler::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
     }
 }
